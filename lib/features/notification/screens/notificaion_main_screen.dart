a2d9f5d8b8475a6f26d91bb44e1410233e9f071f
binii0124 <binii030124@naver.com>
2026-02-24 22:38:00 +0900

    feat: 알림 페이지 진입 시에 새로고침되도록 수정

--- lib/features/notification/screens/notificaion_main_screen.dart
+++ lib/features/notification/screens/notificaion_main_screen.dart
@@ -1,13 +1,35 @@
 // 최초 작성자: 정승빈
 // 최상위 화면 (TabBar와 TabBarView 포함)
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:haenaem/features/notification/views/challenge_invites_view.dart';
 import '../../../core/theme/app_colors.dart';
 import '../../../core/theme/app_typography.dart';
 import '../views/all_notifications_view.dart';
+import '../provider/notification_provider.dart';
 
-class NotificationMainScreen extends StatelessWidget {
+class NotificationMainScreen extends ConsumerStatefulWidget {
   const NotificationMainScreen({super.key});
+
+  @override
+  ConsumerState<NotificationMainScreen> createState() =>
+      _NotificationMainScreenState();
+}
+
+class _NotificationMainScreenState
+    extends ConsumerState<NotificationMainScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // 화면에 진입한 직후 두 탭의 데이터를 최신으로 새로고침합니다.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // 1. '모두' 탭 새로고침
+      ref.read(notificationProvider.notifier).refresh();
+      // 2. '챌린지 초대' 탭 새로고침
+      ref.read(challengeInviteProvider.notifier).fetchInvites();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
