299797b4f8b13a9df5386e836d90e9600500d5d6
sunwook <117016927+nuk0325@users.noreply.github.com>
2026-06-26 20:20:23 +0900

    feat: 관리자 로그인 provider, repository 구현

---
+++ lib/features/admin/data/admin_auth_repository.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'admin_auth_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$adminAuthRepositoryHash() =>
+    r'd19f92823d140585bb95d8c52e382637d0c46b88';
+
+/// See also [adminAuthRepository].
+@ProviderFor(adminAuthRepository)
+final adminAuthRepositoryProvider =
+    AutoDisposeProvider<AdminAuthRepository>.internal(
+      adminAuthRepository,
+      name: r'adminAuthRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$adminAuthRepositoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AdminAuthRepositoryRef = AutoDisposeProviderRef<AdminAuthRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
