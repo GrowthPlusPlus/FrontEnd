7623f066c6a102783b3fe0d30462c806fcbfd974
sunwook <117016927+nuk0325@users.noreply.github.com>
2026-07-09 14:54:07 +0900

    refactor: 공유 다이얼로그 수정 - 아이콘 추가

--- lib/shared/widgets/confirm_dialog.dart
+++ lib/shared/widgets/confirm_dialog.dart
@@ -6,6 +6,7 @@ import './bottom_action_button.dart';
 // 최초 작성자: 강선욱
 
 class ConfirmDialog extends StatelessWidget {
+  final Widget? icon;
   final String? title;
   final String content;
   final String buttonText;
@@ -13,6 +14,7 @@ class ConfirmDialog extends StatelessWidget {
 
   const ConfirmDialog({
     super.key,
+    this.icon,
     this.title,
     required this.content,
     this.buttonText = '확인',
@@ -22,23 +24,23 @@ class ConfirmDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       backgroundColor: Colors.white,
       child: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(24.0),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+            if (icon != null) ...[icon!, const SizedBox(height: 12)],
             if (title != null) ...[
               Text(
                 title!,
-                style: AppTypography.h3.copyWith(color: AppColors.black),
+                style: AppTypography.h2.copyWith(color: AppColors.black),
               ),
-              const SizedBox(height: 12),
             ],
             Text(
               content,
-              style: AppTypography.b1.copyWith(color: AppColors.gray3),
+              style: AppTypography.b3.copyWith(color: AppColors.gray1),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 24),
